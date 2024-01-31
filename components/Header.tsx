75714276d731e33e92754082b9faf857aad94971
erica-l-s <erica.l.s1996@gmail.com>
2024-01-31 17:09:42 -0300

    menu almost done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { Bars3Icon, ChatBubbleLeftIcon, ChevronDownIcon, HomeIcon, PaperAirplaneIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
+import { Bars3Icon, ChatBubbleLeftIcon, ChevronDownIcon, HomeIcon, PaperAirplaneIcon, PhoneIcon, PlayCircleIcon, XMarkIcon } from '@heroicons/react/20/solid';
 import Link from 'next/link'
 import React, { Fragment, useState } from 'react'
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react"
+import { cn } from '@/lib/utils';
 
 const products = [
     {
@@ -80,7 +81,7 @@ export const Header = () => {
                                     {products.map((item) => (
                                         <div key={item.name}
                                             className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50'>
-                                            <div className='flex h-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200'>
+                                            <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200'>
                                                 <item.icon
                                                     className='h-6 w-6 text-[#013894] group-hover:text-blue-600' aria-hidden='true' />
 
@@ -100,16 +101,16 @@ export const Header = () => {
                                     ))}
                                 </div>
                                 <div className='grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50'>
-                                {callsToAction.map((item)=>(
-                                    <a key={item.name}
-                                    href={item.href}
-                                    className='flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-[#013894] hover:bg-gray-100'>
-                                        <item.icon 
-                                        className='h-5 w-5 flex-none text-[#013894]'
-                                        aria-hidden="true"/>
-                                        {item.name}
-                                    </a>
-                                ))}
+                                    {callsToAction.map((item) => (
+                                        <a key={item.name}
+                                            href={item.href}
+                                            className='flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-[#013894] hover:bg-gray-100'>
+                                            <item.icon
+                                                className='h-5 w-5 flex-none text-[#013894]'
+                                                aria-hidden="true" />
+                                            {item.name}
+                                        </a>
+                                    ))}
                                 </div>
                             </Popover.Panel>
                         </Transition>
@@ -120,7 +121,71 @@ export const Header = () => {
                     <a href='#' className='text-sm font-semibold leanding-6 text-white'>Flight + Hotel</a>
                 </Popover.Group>
 
+                <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
+                    <a href='#' className='text-sm font-semibold leading-6 text-white'>
+                        Log in <span aria-hidden='true'>&rarr;</span>
+                    </a>
+                </div>
+
             </nav>
+            <Dialog
+                as='div'
+                className='lg:hidden'
+                open={mobileMenuOpen}
+                onClose={setMobileMenuOpen}
+            >
+                <div className='fixed inset-0 z-10' />
+                <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#013894] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+                    <div className='flex items-center justify-between'>
+                        <a href='#' className='-m-1.5 p-1.5'>
+                            <span className='sr-only'>Booking.com</span>
+                            <img
+                                className='h-4 w-auto'
+                                src='https://upload.wikimedia.org/wikipedia/commons/5/50/Booking.com_Logo_Grey.svg'
+                                alt=''
+                            />
+                        </a>
+                        <button type='button'
+                            className='-m-2.5 rounded-md p-2.5 text-white'
+                            onClick={() => setMobileMenuOpen(false)}>
+                            <span className='sr-only'>Close menu</span>
+                            <XMarkIcon className='h-6 w-6' aria-hidden="true" />
+                        </button>
+
+                    </div>
+                    <div className='mt-6 flow-root'>
+                        <div className='-my-6 divide-y divide-gray-500/10'>
+                            <div className='space-y-2 py-6'>
+                                <Disclosure as='div' className='-mx-3'>
+                                    {({ open }) => (
+                                        <>
+                                            <Disclosure.Button className='flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-blue-800'>
+                                                Stays
+                                                <ChevronDownIcon className={cn(open ? 'rotate-180' : '',
+                                                    "h-5 w-5 flex-none")}
+                                                    aria-hidden='true' />
+                                            </Disclosure.Button>
+                                            <Disclosure.Panel className='mt-2 space-y-2'>
+                                                {[...products, ...callsToAction].map((item) => (
+                                                    <Disclosure.Button
+                                                        key={item.name}
+                                                        as='a'
+                                                        href={item.href}
+                                                        className='block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-blue-800'
+                                                    >
+                                                        {item.name}
+                                                    </Disclosure.Button>
+                                                ))}
+                                            </Disclosure.Panel>
+                                        </>
+                                    )}
+
+                                </Disclosure>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
         </header>
     )
 }
