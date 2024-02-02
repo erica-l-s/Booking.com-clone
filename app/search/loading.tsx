de8882991e900f20862166f3426fa5e8461ac4cb
erica-l-s <erica.l.s1996@gmail.com>
2024-02-02 14:41:13 -0300

    project done

---
+++ app/search/loading.tsx
@@ -0,0 +1,27 @@
+import {Skeleton} from "@/components/ui/skeleton"
+function LoadingResults() {
+  return (
+    <section>
+    <div className="mx-auto max-w-7xl">
+      <p className="text-center animate-pulse font-bold text-[#013894] pt-10">
+        Sit tight - were just scanning the market for the best deals!
+      </p>
+    </div>
+
+    <div className="flex justify-center py-10">
+      <div className="w-10 h-10 bg-[#013894] rounded-full animate-bounce"></div>
+    </div>
+
+    <div className="space-y-2 p-5">
+      {[...Array(10)].map((_, i) => (
+        <div key={i} className="flex space-x-2 mx-auto max-w-7xl">
+          <Skeleton className="h-20 w-20 md:h-44 md:w-44 rounded-lg" />
+          <Skeleton className="h-44 w-full rounded-lg" />
+        </div>
+      ))}
+    </div>
+  </section>
+  )
+}
+
+export default LoadingResults
